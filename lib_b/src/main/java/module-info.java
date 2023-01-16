fb81029e34a9e16075dac0e57837dcfe9cfeb069
gwenlr <gwen.dev@gmail.com>
2023-01-16 23:07:12 +0100

    remove commented code

--- lib_b/src/main/java/module-info.java
+++ lib_b/src/main/java/module-info.java
@@ -1,6 +1,5 @@
 module lib_b {
     exports com.example.resource_module.lib_b;
-    //exports com.example.resource_module.lib_b.dir;
 
     opens com.example.resource_module.lib_b.opened;
     opens com.example.resource_module.lib_b.dir.opened;
